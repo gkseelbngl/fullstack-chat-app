dfb7512a87d941c8c631af46ce7a69c3cfc7c9cb
gkseelbngl <gkseelbngl34@gmail.com>
2025-04-22 20:24:44 +0300

    index js updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,20 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
